4ff0106e06af2bd63bf96ca1fffee7af65eaf333
Jihyun <hyoile81@gmail.com>
2020-12-23 17:17:40 +0900

    중간저장

--- Skyuniversity/src/main/webapp/resources/SQL/pjh.sql
+++ Skyuniversity/src/main/webapp/resources/SQL/pjh.sql
@@ -12,3 +12,11 @@ select count(*)
 from tbl_member
 where memberno='108' and pwd='245976f7bf72702514cd146d10f8b342d7ef30045822fa2c411f62f29e133326';
 
+create table tbl_certificatekind (
+certificateno        NUMBER not null
+, certificatename       varchar2(50) not null
+, charge            number default 0 not null
+, constraint PK_ tbl_certificatekind_no primary key(certificateno)
+);
+
+
